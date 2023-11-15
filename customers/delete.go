2e66fb188bb51540f576bece735e52ad9df3fd3d
caseyaokelly <caseyaokelly@gmail.com>
2023-11-14 20:57:38 -0800

    feat: delete card

--- customers/delete.go
+++ customers/delete.go
@@ -26,3 +26,24 @@ func (s *CustomerService) Delete(id string) error {
 
 	return nil
 }
+
+func (s *CustomerService) DeleteCard(customerId, cardId string) error {
+	s.client.Url.Path = path.Join(s.client.Url.Path, customerId, "cards", cardId)
+
+	req, err := http.NewRequest("DELETE", s.client.Url.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", s.client.ApiKey))
+	req.Header.Add("Accept", "application/json")
+
+	res, err := s.client.HttpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	return nil
+}
