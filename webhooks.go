b52b1369cc676632294faf09074f8d274091fdb1
Bradley Hilton <graywolf336@craftyn.com>
2023-12-11 21:48:10 -0600

    Add more types for webhooks

--- webhooks.go
+++ webhooks.go
@@ -1,14 +1,27 @@
 package payarc
 
-import "encoding/json"
-
 type WebhookEventType string
 
 var (
-	WebhookEventTypeChargeCreated WebhookEventType = "Charges Created"
+	WebhookEventTypeChargeCreated   WebhookEventType = "Charges Created"
+	WebhookEventTypeChargeCaptured  WebhookEventType = "Charge Captured"
+	WebhookEventTypeChargeVoided    WebhookEventType = "Charge Voided"
+	WebhookEventTypeChargeFailed    WebhookEventType = "Charge Failure"
+	WebhookEventTypeCustomerCreated WebhookEventType = "Customers Created"
+	WebhookEventTypeCustomerUpdated WebhookEventType = "Customers Updated"
+	WebhookEventTypeCustomerDeleted WebhookEventType = "Customers Deleted"
+	WebhookEventTypeCardCreated     WebhookEventType = "Card Created"
+	WebhookEventTypeCardUpdated     WebhookEventType = "Card Updated"
+	WebhookEventTypeCardDeleted     WebhookEventType = "Card Deleted"
 )
 
-type Event struct {
-	RequestPayload json.RawMessage `json:"request_payload"`
-	ApiResponse    json.RawMessage `json:"api_response"`
+type WebhookEvent struct {
+	RequestPayload string           `json:"request_payload"`
+	ApiResponse    string           `json:"api_response"`
+	Type           WebhookEventType `json:"event_type"`
+}
+
+type WebhookEventChargeResponse struct {
+	Headers        map[string]string `json:"headers"`
+	ChargeResponse ChargeResponse    `json:"original"`
 }
